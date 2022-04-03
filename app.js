0482af7f2bfcc6ce1cfadc11da090e962ed5ba3c
unknown <zachariah11jerobert@gmail.com>
2022-04-03 08:32:26 +0530

    initial file setup & checked postman colection

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+// app.get('/api/v1/tasks')        -get All the tasks
+// app.post('/api/v1/tasks')       -create a new task
+// app.get('/api/v1/tasks/:id')    -get single task
+// app.patch('/api/v1/tasks/:id')  -update task
+// app.delete('/api/v1/tasks/:id') -delete task
+
+const port = 3000;
+
+app.listen(port, console.log(`server is listening on port ${port}`));
---
+++ controllers/tasks.js
@@ -0,0 +1,28 @@
+const getAllTasks = (req, res) => {
+  res.send("get all tasks");
+};
+
+const createTask = (req, res) => {
+  req.body.action = "create task";
+  res.json(req.body);
+};
+
+const getTask = (req, res) => {
+  res.json({ action: "get single task", id: req.params.id });
+};
+
+const updateTask = (req, res) => {
+  res.send("update task");
+};
+
+const deleteTask = (req, res) => {
+  res.send("delete task");
+};
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
